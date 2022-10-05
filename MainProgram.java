a4436db7bf25b13a057307492ebecfcc238a5726
ValeriaBelova <114664655+ValeriaBelova@users.noreply.github.com>
2022-10-05 23:01:17 +0200

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,11 @@
+package Sprint1.Inlämningsuppgift1Växter;
+
+
+public class MainProgram{
+
+    public static void main(String[] args) {
+        PrintOutTools printOutTools = new PrintOutTools();
+        printOutTools.printMe(printOutTools.CheckinPlants());
+
+    }
+}
